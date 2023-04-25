144362d5f1ac605c716d724d4f94bec06a768600
ckaesar <a502165083@126.com>
2023-04-25 14:25:54 +0800

    更新 .gitignore

---
+++ practice/src/main/java/com/kaesar/kinds_of_problems/practice/PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.kaesar.kinds_of_problems.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeApplication.class, args);
+    }
+
+}
---
+++ practice/src/test/java/com/kaesar/kinds_of_problems/practice/PracticeApplicationTests.java
@@ -0,0 +1,12 @@
+package com.kaesar.kinds_of_problems.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
